7347403b3ac854b83dd71daa4cc37f483d6516ed
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2024-03-22 15:07:39 +0500

    Beginner misollari

--- index.php
+++ index.php
@@ -3,18 +3,78 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Maxsus gruh 1-amalyot</title>
+    <title>Maxsus gruh 1-amalyot Beginner</title>
 </head>
 <body>
-    <h1>Begin 1-misol</h1>
+    <!-- <h1>Begin 1-2-misol</h1>
     <?php
     $a = $_GET['a'];
     $p=4*$a;
+    $s=$a*$a;
+  ?>
+  <h2>Kvadrat tomoni <?= $_GET['a']?> Perimetri : <?= $p?> Yuzasi : <?= $s?></h2> -->
+  <!-- <h1>Begin 3-misol</h1>
+  <?php
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    $p=2*($a+$b);
+    $s=$a*$b;
+  ?>
+  <h2>Tog'ri to'rtburchak tomonlari <?= $_GET['a']?>,<?= $_GET['b']?> Perimetri : <?= $p?> Yuzasi : <?= $s?></h2> -->
+  <!-- <h1>Begin 4-misol</h1>
+  <?php
+    $d = $_GET['d'];
+    $PI=3.14;
+    $l= 2*$PI*$d/2;
+  ?>
+  <h2>Aylana deametri <?= $_GET['d']?> Uzunligi : <?= $l?></h2> -->
+  <!-- <h1>Begin 5-misol</h1>
+  <?php
+    $a = $_GET['a'];
+    $V=pow($a,3);
+    $S= 6*$a*$a;
+  ?>
+  <h2>Kub tomini <?= $_GET['a']?> hajmi : <?= $V?> To'la sirt yuzasi <?= $S?></h2> -->
+  <!-- <h1>Begin 6-misol</h1>
+  <?php
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    $c = $_GET['c'];
+    $V=$a*$b*$c;
+    $S= 2*($a*$b+$b*$c+$a*$c);
+  ?>
+  <h2>Parallapiped tomonlari  <?= $_GET['a'],$_GET['b'],$_GET['c']?> hajmi : <?= $V?> To'la sirt yuzasi <?= $S?></h2> <h1>Begin 5-misol</h1> -->
+ <!-- <h1>Begin 7-misol</h1> 
+    <?php
+    $r = $_GET['r'];
+    $pi=3.14;
+    $l=2*$pi*$r;
+    $S= $pi*$r*$r;
+  ?>
+  <h2>Doira radiosi <?= $_GET['r']?> uzunligi : <?= $l?> yuzasi <?= $S?></h2>  -->
+  <!-- <h1>Begin 8-misol</h1>
+    <?php
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    $u=($a+$b)/2;
     
   ?>
-  <h2>Kvadrat tomoni :<?= $_GET['a']?> : Perimetri : <?= $p?></h2>
+  <h2>Ikkita son  <?= $_GET['a']?>,<?= $_GET['b']?> o'rta arifmetigi : <?= $u?></h2>  -->
+  <!-- <h1>Begin 9-misol</h1>
+    <?php
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    $u=sqrt($a*$b);
     
-    
-    ?>
-</body>
+  ?>
+  <h2>Ikkita son  <?= $_GET['a']?>,<?= $_GET['b']?> o'rta geometrigi : <?= $u?></h2>  -->
+  <!-- <h1>Begin 10-misol</h1>
+  <?php
+  $a = $_GET['a'];
+  $b = $_GET['b'];
+  $y=$a+$b;
+  $k=$a*$b;
+  ?>
+  <h2>Sonlar <?=$_GET['a']?>,<?=$_GET['b']?> yig'indisi : <?=$y?> ko'paytmasi <?= $k?> kvadratlari <?= $a*$a,$b*$b?></h2>-->
+</body> 
 </html>
