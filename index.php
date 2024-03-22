235a25e885331cac8b211cdbd5bb2e0be55511a3
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2024-03-22 14:50:15 +0500

    Begin 1-misol

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maxsus gruh 1-amalyot</title>
+</head>
+<body>
+    <h1>Begin 1-misol</h1>
+    <?php
+    $a = $_GET['a'];
+    $p=4*$a;
+    
+  ?>
+  <h2>Kvadrat tomoni :<?= $_GET['a']?> : Perimetri : <?= $p?></h2>
+    
+    
+    ?>
+</body>
+</html>
